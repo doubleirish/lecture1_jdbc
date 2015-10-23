bdc1273f6cf5df35223a035a3d231e91fc4c74d9
doubleirish <conor.redmond@gmail.com>
2015-10-22 17:34:09 -0700

    add comment

--- src/main/java/edu/uw/data/app/App7UsersSpring.java
+++ src/main/java/edu/uw/data/app/App7UsersSpring.java
@@ -12,9 +12,19 @@ import java.util.List;
 /**
  * Hello spring application context with spring Jdbc template!
  *   we can run this from the command line
- *   1)
- *   2)
- *   3) java -jar target/lecture1_jdbc-1.0-SNAPSHOT.jar
+ *   1) start the standalone derby service
+ *      cd DERBY_HOME\bin
+ *      ./startNetworkServer
+ *
+ *   2) populate the database using sql squirrel and the attached all.sql script
+ *
+ *   3) Build this application as an exectuable jar
+ *           mvn clean package -DskipTests spring-boot:repackage
+ *
+ *   4) run the application to connect to the remote server
+ *      java -jar target/lecture1_jdbc-1.0-SNAPSHOT.jar
+ *
+ *   5) you should see user information printed out
  */
 public class App7UsersSpring
 {
